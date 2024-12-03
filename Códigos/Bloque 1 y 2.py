d233eaad2942d874d50e929a3d0b5f73abaa9cf6
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-02 23:06:50 -0600

    Update Bloque 1 y 2.py

--- Códigos/Bloque 1 y 2.py
+++ Códigos/Bloque 1 y 2.py
@@ -1,1 +1,6 @@
+Ejercicio 1.1 Crea un programa que muestre tu nombre. 
+# Programa 1.1 Programa que imprime un mensaje  de texto en la pantalla 
+# Fecha: 2024/10/14
+# Elaborado por: Jazmin Macias Sabas 
+print("Hola , mi nombre es Jazmin Macias Sabas")
 
